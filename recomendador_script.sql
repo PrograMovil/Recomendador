dde4ca73c9e9f40774dd5e1bbec077340fd9595f
SheshoVega <hvega.sergio@gmail.com>
2017-05-10 14:08:34 -0600

    actualizado el script sql

--- recomendador_script.sql
+++ recomendador_script.sql
@@ -16,48 +16,15 @@ CREATE SCHEMA IF NOT EXISTS `recomendador` DEFAULT CHARACTER SET utf8 ;
 USE `recomendador` ;
 
 -- -----------------------------------------------------
--- Table `recomendador`.`albumes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `recomendador`.`albumes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(100) NULL,
-  `anio` INT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `recomendador`.`generos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `recomendador`.`generos` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `recomendador`.`canciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `recomendador`.`canciones` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(60) NULL,
   `artista` VARCHAR(45) NULL,
-  `albumes_id` INT NOT NULL,
-  `generos_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_canciones_albumes_idx` (`albumes_id` ASC),
-  INDEX `fk_canciones_generos1_idx` (`generos_id` ASC),
-  CONSTRAINT `fk_canciones_albumes`
-    FOREIGN KEY (`albumes_id`)
-    REFERENCES `recomendador`.`albumes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_canciones_generos1`
-    FOREIGN KEY (`generos_id`)
-    REFERENCES `recomendador`.`generos` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `album` VARCHAR(100) NULL,
+  `genero` VARCHAR(50) NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
