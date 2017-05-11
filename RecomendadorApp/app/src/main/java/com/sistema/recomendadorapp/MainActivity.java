04e6367de818f9eea1bb32cb7d060e2724a6a28f
SheshoVega <hvega.sergio@gmail.com>
2017-05-10 19:38:11 -0600

    libs y ListView started

--- RecomendadorApp/app/src/main/java/com/sistema/recomendadorapp/MainActivity.java
+++ RecomendadorApp/app/src/main/java/com/sistema/recomendadorapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sistema.recomendadorapp;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.view.View;
 import android.widget.TextView;
@@ -18,6 +23,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+
+import Model.Cancion;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
         SearchView searchView = (SearchView) findViewById(R.id.search);
         searchView.setQueryHint("Buscar canciones..");
         textView = (TextView) findViewById(R.id.test);
+
+//        ListView list = (ListView) findViewById(R.id.lista_canciones);
+//        final ListViewAdapter listAdapter = new ListViewAdapter(this, arrayList);
+//        list.setAdapter(listAdapter);
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -58,6 +71,55 @@ public class MainActivity extends AppCompatActivity {
 //        urlRequest = urlBase + "action=Testing";
 //        ***********************************************************
 
+    }
+
+    class ListViewAdapter  extends BaseAdapter {
+        Context context;
+        LayoutInflater inflater;
+        private ArrayList<Cancion> cancioneslist;
+
+        public ListViewAdapter(Context context, ArrayList<Cancion> cancionesList) {
+            this.context = context;
+            this.cancioneslist = cancionesList;
+            inflater = LayoutInflater.from(context);
+            this.cancioneslist = new ArrayList<Cancion>();
+        }
+
+        class ViewHolder {
+            TextView nombre;
+        }
+
+        @Override
+        public int getCount() {
+            return this.cancioneslist.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return this.cancioneslist.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder();
+                convertView = inflater.inflate(R.layout.list_item, null);
+                // Locate the TextViews in list_items.xml
+                holder.nombre = (TextView) convertView.findViewById(R.id.nombre_cancion);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+            // Set the results into TextViews
+            holder.nombre.setText(cancioneslist.get(position).getNombre());
+            return convertView;
+        }
     }
 
     public class BuscarTask extends AsyncTask<String, Void, String> {
